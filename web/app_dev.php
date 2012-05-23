e6dbb5b9fd3c076ce0129314221d781985da1049
butt <olkov5@ukr.net>
2012-05-23 14:31:29 +0300

    [+] ..

--- web/app_dev.php
+++ web/app_dev.php
@@ -11,6 +11,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array(
         '127.0.0.1',
         '::1',
+        '94.45.65.48'
     ))
 ) {
     header('HTTP/1.0 403 Forbidden');
